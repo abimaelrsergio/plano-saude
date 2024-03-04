799db2d76a7f6a0904954c07f41041b609a5a4af
abimaelrsergio <abimaelr.sergio@gmail.com>
2024-03-04 16:51:15 -0300

    Adicionado teste

--- src/test/java/com/plano/saude/cadastro/CadastroApplicationTests.java
+++ src/test/java/com/plano/saude/cadastro/CadastroApplicationTests.java
@@ -1,13 +1,25 @@
 package com.plano.saude.cadastro;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CadastroApplicationTests {
 
+	@Autowired
+	TestRestTemplate restTemplate;
+
 	@Test
-	void contextLoads() {
+	void getInfo() {
+		ResponseEntity<String> response = restTemplate
+		.withBasicAuth("abimael","abimael")
+		.getForEntity("/plano-saude/beneficiarios", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 
 }
